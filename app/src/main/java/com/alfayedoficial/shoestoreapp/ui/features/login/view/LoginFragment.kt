f3acba7d490673ce9fcb705ec6466fcde43cad65
alfayedoficial <alialfayed1@gmail.com>
2022-08-15 11:07:25 +0200

    fragment_login.xml

---
+++ app/src/main/java/com/alfayedoficial/shoestoreapp/ui/features/login/view/LoginFragment.kt
@@ -0,0 +1,45 @@
+package com.alfayedoficial.shoestoreapp.ui.features.login.view
+
+import com.alfayedoficial.kotlinutils.kuTakeFocus
+import com.alfayedoficial.shoestoreapp.R
+import com.alfayedoficial.shoestoreapp.core.common.fragment.BaseFragment
+import com.alfayedoficial.shoestoreapp.databinding.FragmentLoginBinding
+import com.alfayedoficial.shoestoreapp.utils.ValidationInputs.validationAuthTextInputEditText
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class LoginFragment : BaseFragment<FragmentLoginBinding>() {
+
+
+    fun takeFocus(){
+        kuTakeFocus(dataBinder.lyParent)
+    }
+
+    fun onLoginClicked() {
+        dataBinder.apply {
+            if (validationAuthTextInputEditText(Pair(1, emailEt), Pair(2, passwordEt))) {
+                navigate()
+            }
+        }
+    }
+
+    fun onLoginWithAnExistingAccountClicked(){
+        navigate()
+    }
+
+    private fun navigate() = navController.navigate(R.id.action_loginFragment_to_onboardFragment)
+
+
+    override val layoutResourceLayout: Int
+        get() = R.layout.fragment_login
+
+    override fun onFragmentCreated(dataBinder: FragmentLoginBinding) {
+        dataBinder.apply {
+            fragment = this@LoginFragment
+            lifecycleOwner = this@LoginFragment
+        }
+    }
+
+    override fun setUpViewModelStateObservers() { println("login fragment")}
+}
